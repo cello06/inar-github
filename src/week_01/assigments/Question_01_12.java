c064dfcf6afafd6a4bb77988d0e51f66ad15ed67
unknown <celalettinakturk06@gmail.com>
2023-08-06 22:31:02 +0300

    week_01

---
+++ src/week_01/assigments/Question_01_12.java
@@ -0,0 +1,12 @@
+package week_01.assigments;
+
+public class Question_01_12 {
+    public static void main(String[] args) {
+        System.out.println("Distance in miles :");
+        System.out.println("24.0");
+        System.out.println("Time in minutes");
+        System.out.println("100 minutes 35 seconds\n"
+                + "Average speed in kilometers per hour :");
+        System.out.println((24.00 * 1.60) / (100.00 / 60.00 + (35.00 / (60.00 * 60.00))));
+    }
+}
---
+++ src/week_01/live_class/Subtraction.java
@@ -0,0 +1,8 @@
+package week_01.live_class;
+
+public class Subtraction {
+    public static void main(String[] args) {
+        System.out.println("Cl" + "fdd");
+
+    }
+}
