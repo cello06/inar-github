2e2e4013f4c8329d4d2a4f63e8bee81fb32ed8dc
Celalettin harzemşah Aktürk <celalettinakturk06@gmail.com>
2023-08-21 16:50:35 +0300

    Assigments of week_03

---
+++ src/week_03/assigments/Question_03_22.java
@@ -0,0 +1,16 @@
+package week_03.assigments;
+
+import java.util.Scanner;
+
+public class Question_03_22 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a point with two coordinates: ");
+        double coordinate1 = input.nextDouble();
+        double coordinate2 = input.nextDouble();
+        double distance = Math.pow(Math.pow((coordinate1 - 0), 2) + Math.pow((coordinate2 - 0), 2), 0.5);
+        if (distance > 10) {
+            System.out.println("Point (" + coordinate1 + "," + coordinate2 + ") is not in the circle");
+        } else System.out.println("Point (" + coordinate1 + "," + coordinate2 + ") is  in the circle");
+    }
+}
