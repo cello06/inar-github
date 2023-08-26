f5cb4168a31dcaa5ad9a509d63d45f091ebb7da9
Celalettin harzemşah Aktürk <celalettinakturk06@gmail.com>
2023-08-26 09:49:28 +0300

    Assigments of week_04

---
+++ src/week_04/assigments/Question_04_04.java
@@ -0,0 +1,19 @@
+package week_04.assigments;
+
+import java.util.Scanner;
+
+public class Question_04_04 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the side : ");
+
+        double sideOfHexagon = input.nextDouble();
+
+        double area = (6 * Math.pow(sideOfHexagon, 2))
+                / (4 * Math.tan(Math.PI / 6));
+
+        System.out.printf("The area of the hexagon is %4.2f", area);
+    }
+}
