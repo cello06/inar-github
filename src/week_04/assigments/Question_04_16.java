5feeb186979c7681ecbad7a3a60b077d5430734a
Celalettin harzemşah Aktürk <celalettinakturk06@gmail.com>
2023-08-27 21:06:19 +0300

    Assigments of week_04

--- src/week_04/assigments/Question_04_16.java
+++ src/week_04/assigments/Question_04_16.java
@@ -4,7 +4,12 @@ import java.util.Scanner;
 
 public class Question_04_16 {
     public static void main(String[] args) {
-        Scanner input=new Scanner(System.in);
 
+        Scanner input = new Scanner(System.in);
+
+        int unicodeOfUpperCaseLetter = (int) (Math.random() * (90 - 65)) + 65;
+
+        System.out.print((char) (unicodeOfUpperCaseLetter));
+        
     }
 }
