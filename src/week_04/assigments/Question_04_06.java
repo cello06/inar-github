0ef05b821fe6017d86d97ec4caf742c47c5a20f9
Celalettin harzemşah Aktürk <celalettinakturk06@gmail.com>
2023-08-27 06:31:05 +0300

    Assigments of week_04

--- src/week_04/assigments/Question_04_06.java
+++ src/week_04/assigments/Question_04_06.java
@@ -4,27 +4,62 @@ import java.util.Scanner;
 
 public class Question_04_06 {
     public static void main(String[] args) {
-        double angle1=(Math.random()*Math.PI)*2;
-        double angle2=(Math.random()*Math.PI)*2;
-        double angle3=(Math.random()*Math.PI)*2;
 
-        double xOfPoint1=40*Math.cos(angle1);
-        double yOfPoint1=40*Math.sin(angle1);
-        double xOfPoint2=40*Math.cos(angle2);
-        double yOfPoint2=40*Math.sin(angle2);
-        double xOfPoint3=40*Math.cos(angle3);
-        double yOfPoint3=40*Math.sin(angle3);
+        double angle1 = (Math.random() * Math.PI) * 2;
 
-        double sideABetweenPoint1AndPoint2=Math.sqrt(Math.pow(xOfPoint1-xOfPoint2,2)+Math.pow(yOfPoint1-yOfPoint2,2));
-        double sideBBetweenPoint2AndPoint3=Math.sqrt(Math.pow(xOfPoint2-xOfPoint3,2)+Math.pow(yOfPoint2-yOfPoint3,2));
-        double sideCBetweenPoint1AndPoint3=Math.sqrt(Math.pow(xOfPoint1-xOfPoint3,2)+Math.pow(yOfPoint1-yOfPoint3,2));
+        double angle2 = (Math.random() * Math.PI) * 2;
 
-        double angleA=Math.acos((sideABetweenPoint1AndPoint2*sideABetweenPoint1AndPoint2-sideBBetweenPoint2AndPoint3*sideBBetweenPoint2AndPoint3-sideCBetweenPoint1AndPoint3*sideCBetweenPoint1AndPoint3)/(-2*sideBBetweenPoint2AndPoint3*sideCBetweenPoint1AndPoint3));
-        double angleB=Math.acos((sideBBetweenPoint2AndPoint3*sideBBetweenPoint2AndPoint3-sideABetweenPoint1AndPoint2*sideABetweenPoint1AndPoint2-sideCBetweenPoint1AndPoint3*sideCBetweenPoint1AndPoint3)/(-2*sideABetweenPoint1AndPoint2*sideCBetweenPoint1AndPoint3));
-        double angleC=Math.acos((sideCBetweenPoint1AndPoint3*sideCBetweenPoint1AndPoint3-sideBBetweenPoint2AndPoint3*sideBBetweenPoint2AndPoint3-sideABetweenPoint1AndPoint2*sideABetweenPoint1AndPoint2)/(-2*sideABetweenPoint1AndPoint2*sideBBetweenPoint2AndPoint3));
+        double angle3 = (Math.random() * Math.PI) * 2;
 
-        System.out.println("The three angles are : "+angleA+" "+angleB+" "+angleC);
 
+        double xOfPoint1 = 40 * Math.cos(angle1);
+        double yOfPoint1 = 40 * Math.sin(angle1);
+
+
+        double xOfPoint2 = 40 * Math.cos(angle2);
+        double yOfPoint2 = 40 * Math.sin(angle2);
+
+
+        double xOfPoint3 = 40 * Math.cos(angle3);
+        double yOfPoint3 = 40 * Math.sin(angle3);
+
+
+
+
+        double sideABetweenPoint1AndPoint2 = Math.sqrt(Math.pow(xOfPoint1 - xOfPoint2, 2)
+                + Math.pow(yOfPoint1 - yOfPoint2, 2));
+
+
+        double sideBBetweenPoint2AndPoint3 = Math.sqrt(Math.pow(xOfPoint2 - xOfPoint3, 2)
+                + Math.pow(yOfPoint2 - yOfPoint3, 2));
+
+
+        double sideCBetweenPoint1AndPoint3 = Math.sqrt(Math.pow(xOfPoint1 - xOfPoint3, 2)
+                + Math.pow(yOfPoint1 - yOfPoint3, 2));
+
+
+
+
+
+        double angleA = Math.acos((sideABetweenPoint1AndPoint2 * sideABetweenPoint1AndPoint2
+                - sideBBetweenPoint2AndPoint3 * sideBBetweenPoint2AndPoint3
+                - sideCBetweenPoint1AndPoint3 * sideCBetweenPoint1AndPoint3)
+                / (-2 * sideBBetweenPoint2AndPoint3 * sideCBetweenPoint1AndPoint3));
+
+
+        double angleB = Math.acos((sideBBetweenPoint2AndPoint3 * sideBBetweenPoint2AndPoint3
+                - sideABetweenPoint1AndPoint2 * sideABetweenPoint1AndPoint2
+                - sideCBetweenPoint1AndPoint3 * sideCBetweenPoint1AndPoint3)
+                / (-2 * sideABetweenPoint1AndPoint2 * sideCBetweenPoint1AndPoint3));
+
+
+        double angleC = Math.acos((sideCBetweenPoint1AndPoint3 * sideCBetweenPoint1AndPoint3
+                - sideBBetweenPoint2AndPoint3 * sideBBetweenPoint2AndPoint3
+                - sideABetweenPoint1AndPoint2 * sideABetweenPoint1AndPoint2)
+                / (-2 * sideABetweenPoint1AndPoint2 * sideBBetweenPoint2AndPoint3));
+
+
+        System.out.printf("The three angles are : %5.2f  %5.2f  %5.2f" , Math.toDegrees(angleA), Math.toDegrees(angleB) , Math.toDegrees(angleC));
 
 
     }
---
+++ src/week_04/assigments/Question_04_07.java
@@ -0,0 +1,10 @@
+package week_04.assigments;
+
+import java.util.Scanner;
+
+public class Question_04_07 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+    }
+}
