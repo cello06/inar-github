9c6d92fe0c95dd5196f88b63bfab74c9cf30ec49
Celalettin harzemşah Aktürk <celalettinakturk06@gmail.com>
2023-09-02 10:22:27 +0300

    case_studies of week_05

---
+++ src/week_05/live_class/PrimeNumber.java
@@ -0,0 +1,53 @@
+package week_05.live_class;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+
+        String listOfPrimeNumber = "";
+
+
+        int count=0;
+
+        int number = 2;
+
+
+
+        while(count<50){
+
+            boolean isPrimeNumber=true;
+
+
+            for(int i = 2 ; i<=number/2;i++){
+
+                if(number%i==0){
+
+                    isPrimeNumber=false;
+                    break;
+                }
+            }
+
+
+
+            if(isPrimeNumber){
+
+                if(count%10==0){
+
+                    listOfPrimeNumber+="\n"+number;
+
+                }else {
+
+                    listOfPrimeNumber+=" "+number;
+
+                }
+
+                count++;
+
+            }
+
+            number++;
+
+        }
+
+        System.out.println(listOfPrimeNumber);
+    }
+}
