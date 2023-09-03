4cf669db5c23a3142c3fbfbee166e1acec9ed84f
Celalettin harzemşah Aktürk <celalettinakturk06@gmail.com>
2023-09-03 16:49:03 +0300

    Assigments of week_05

--- src/week_05/assigments/Question_05_16.java
+++ src/week_05/assigments/Question_05_16.java
@@ -35,7 +35,7 @@ public class Question_05_16 {
         int lastIndexOfComma = smallestFactorsOfNumber.lastIndexOf(',');
 
         smallestFactorsOfNumber = smallestFactorsOfNumber.substring(0, lastIndexOfComma) + ".";
-        
+
         System.out.println(smallestFactorsOfNumber);
     }
 }
