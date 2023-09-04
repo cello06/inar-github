f2bd145908d7d840f39833df7db03b738ac09073
Celalettin harzemşah Aktürk <celalettinakturk06@gmail.com>
2023-09-04 05:34:52 +0300

    Assigments of week_05

--- src/week_05/assigments/Question_05_18.java
+++ src/week_05/assigments/Question_05_18.java
@@ -4,7 +4,7 @@ public class Question_05_18 {
     public static void main(String[] args) {
         for(int i=1;i<=6;i++){
             for(int j=1;j<=i;j++){
-                System.out.print(j);
+                System.out.print(j+" ");
             }
             System.out.println();
         }
@@ -12,7 +12,7 @@ public class Question_05_18 {
 
         for (int i=6;i>=1;i--){
             for(int j=1;j<=i;j++){
-                System.out.print(j);
+                System.out.print(j+" ");
             }
             System.out.println();
         }
@@ -20,10 +20,10 @@ public class Question_05_18 {
 
         for(int i=1;i<=6;i++){
             for(int j=1;j<=(6-i);j++){
-                System.out.print(" ");
+                System.out.print("   ");
             }
             for (int l =i;l>=1;l--){
-                System.out.print(l);
+                System.out.print(" "+l+" ");
             }
             System.out.println();
         }
@@ -31,10 +31,10 @@ public class Question_05_18 {
 
         for (int i = 6;i>=1;i--){
             for(int j=6;j>i;j--){
-                System.out.print(" ");
+                System.out.print("  ");
             }
             for (int k=1;k<=i;k++){
-                System.out.print(k);
+                System.out.print(k+" ");
             }
             System.out.println();
 
