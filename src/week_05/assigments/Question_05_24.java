a4f070d2994b1d9fbc904b5e29e692e1e5177f67
Celalettin harzemşah Aktürk <celalettinakturk06@gmail.com>
2023-09-04 18:56:01 +0300

    Assigments of week_05

---
+++ src/week_05/assigments/Question_05_24.java
@@ -0,0 +1,17 @@
+package week_05.assigments;
+
+import java.util.Scanner;
+
+public class Question_05_24 {
+    public static void main(String[] args) {
+
+        double sum =0;
+        
+        for(int i = 1 ; i<=97 ; i+=2){
+            sum+=(i*1.0)/(i+2);
+        }
+        System.out.println("Sum of series : "+sum);
+
+
+    }
+}
