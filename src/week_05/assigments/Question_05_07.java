bd01553098963d0da7fe00ccdbbeb9e2c22b5360
Celalettin harzemşah Aktürk <celalettinakturk06@gmail.com>
2023-09-06 08:21:12 +0300

    Assigments of week_05

--- src/week_05/assigments/Question_05_07.java
+++ src/week_05/assigments/Question_05_07.java
@@ -4,25 +4,29 @@ public class Question_05_07 {
     public static void main(String[] args) {
 
         double tuition = 10_000;
-
-        double sumOfFourYearsCost=0;
+        double tuitionToShow = 0;
+        double sumOfFourYearsCost = 0;
 
         int year;
 
 
+        for (year = 1; year <= 15; year++) {
 
-        for(year=1;year<=10;year++){
-
-            if(year<=4) {
+            if (year >= 12) {
 
                 sumOfFourYearsCost += tuition;
             }
+            if (year <= 11) {
+                tuitionToShow=tuition;
 
-            tuition+=(tuition/100)*5;
+            }
+
+            tuition += (tuition / 100) * 5;
         }
+        System.out.printf("Tuition in ten years : $%.0f\n",tuitionToShow);
+        System.out.printf("Total cost for four years' worth of tuition after the : "
+                +" tenth year : $%.0f",sumOfFourYearsCost);
 
 
-        System.out.printf("After four years the total cost is $%.3f"+
-                "\nAfter 10 years the tuition will be $%.3f",sumOfFourYearsCost,tuition);
     }
 }
---
+++ src/week_05/assigments/Question_05_31.java
@@ -0,0 +1,7 @@
+package week_05.assigments;
+
+public class Question_05_31 {
+    public static void main(String[] args) {
+
+    }
+}
