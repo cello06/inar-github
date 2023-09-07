d28695894f10b391fef6077cef3cd593d566eb25
Celalettin harzemşah Aktürk <celalettinakturk06@gmail.com>
2023-09-07 12:25:09 +0300

    Assigments of week_05

--- src/week_05/assigments/Question_05_38.java
+++ src/week_05/assigments/Question_05_38.java
@@ -1,7 +1,36 @@
 package week_05.assigments;
 
+import java.util.Scanner;
+
 public class Question_05_38 {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a decimal integer : ");
+
+        int decimalNumber = input.nextInt();
+
+        int quotient = decimalNumber;
+
+
+        String octalNumber = "";
+
+        String remainder = "";
+
+
+        while (quotient != 0) {
+
+            remainder = Integer.toString(quotient % 8);
+
+            quotient = quotient / 8;
+
+            octalNumber = remainder.concat(octalNumber);
+
+        }
+
+        System.out.printf("The octal of %d is %s", decimalNumber, octalNumber);
+
 
     }
 }
