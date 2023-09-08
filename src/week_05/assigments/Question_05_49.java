42679e0ee7c6b38574b149040da5907b6407b0a3
Celalettin harzemşah Aktürk <celalettinakturk06@gmail.com>
2023-09-08 16:02:05 +0300

    Assigments of week_05

---
+++ src/week_05/assigments/Question_05_49.java
@@ -0,0 +1,45 @@
+package week_05.assigments;
+
+import java.util.Scanner;
+
+public class Question_05_49 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a string : ");
+
+        String yourStr = input.nextLine();
+
+        yourStr = yourStr.toLowerCase();
+
+
+        int numberOfVowels = 0;
+        int numberOfConsonants = 0;
+
+
+        for (int i = 0; i <= yourStr.length() - 1; i++) {
+
+            if (yourStr.charAt(i) == 'a'
+                    || yourStr.charAt(i) == 'e'
+                    || yourStr.charAt(i) == 'i'
+                    || yourStr.charAt(i) == 'o'
+                    || yourStr.charAt(i) == 'u') {
+
+                numberOfVowels++;
+
+            } else if (yourStr.charAt(i) == ' ') {
+
+                continue;
+
+            } else {
+
+                numberOfConsonants++;
+            }
+
+        }
+
+        System.out.printf("The number of vowels is %d\n" +
+                "The number of consonants is %d", numberOfVowels, numberOfConsonants);
+    }
+}
