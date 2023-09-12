4a84ffa03c9580132473ce4d1472a41e1f3c9d2d
Celalettin harzemşah Aktürk <celalettinakturk06@gmail.com>
2023-09-12 19:31:13 +0300

    thanks to musa I corrected my fault

--- src/week_05/assigments/Question_05_25.java
+++ src/week_05/assigments/Question_05_25.java
@@ -5,19 +5,14 @@ public class Question_05_25 {
         double sum = 0;
 
         for (int i = 1; i <= 100_000; i++) {
-            sum += (Math.pow((-1), i + 1)) / ((2 * i) - 1);
+            sum += 4*(Math.pow((-1), i + 1)) / ((2 * i) - 1);
 
-            if (i == 10_000) {
-                double sumOfFirstI = sum * 4;
-                System.out.println("PI value for i=10_000 : " + sumOfFirstI);
-            }
-            if (i == 20_000) {
-                double sumOfSecondI = sum * 4;
-                System.out.println("PI value for i=20_000 : " + sumOfSecondI);
+            if (i % 10_000==0) {
+                System.out.println("PI value for i = : " + i+" : "+sum);
             }
 
         }
-        sum *= 4;
+
         System.out.println("PI value for i=100_000 : " + sum);
 
 
