8227d7e3c35dc466a77ba5e426b6160fdcac4e74
Celalettin harzemşah Aktürk <celalettinakturk06@gmail.com>
2023-09-15 18:55:20 +0300

    Assignments of week_06

--- src/week_06/assignments/Question_06_02.java
+++ src/week_06/assignments/Question_06_02.java
@@ -1,5 +1,25 @@
 package week_06.assignments;
 
-public class Question_06_02 {
+import java.util.Scanner;
 
+public class Question_06_02 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a integer :");
+        long numberOfUser = input.nextLong();
+
+        int sumOfDigits = sumDigits(numberOfUser);
+        System.out.printf("The sum of the digits in %d is %d", numberOfUser, sumOfDigits);
+    }
+
+    public static int sumDigits(long number) {
+        long sumOfDigits = 0;
+
+        while (number > 0) {
+            sumOfDigits += number % 10;
+            number = number / 10;
+        }
+        return (int) sumOfDigits;
+    }
 }
