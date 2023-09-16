1fcd72134031d62f53b5e851c5971beaa9d7188f
Celalettin harzemşah Aktürk <celalettinakturk06@gmail.com>
2023-09-16 13:43:32 +0300

    Assignments of week_06

--- src/week_06/assignments/Question_06_22.java
+++ src/week_06/assignments/Question_06_22.java
@@ -1,10 +1,33 @@
 package week_06.assignments;
 
+import java.sql.SQLOutput;
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Question_06_22 {
     public static void main(String[] args) {
-        String a = "celaletin";
-        System.out.println(a.toUpperCase(Locale.ROOT));
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number : ");
+        long number = input.nextLong();
+
+        double squareOfNumber = sqrt(number);
+        System.out.printf("The approximated square root of %d is %.1f", number, squareOfNumber);
+    }
+
+    public static double sqrt(long number) {
+        double nextGuess = 0;
+        double lastGuess = 0;
+        double approximateSquare = 1;
+
+        do {
+            lastGuess = approximateSquare;
+            nextGuess = (lastGuess + number / lastGuess) / 2;
+            approximateSquare = nextGuess;
+
+        } while ((Math.abs(nextGuess - lastGuess)) >= 0.000000001E-10);
+
+        return approximateSquare;
+
     }
 }
