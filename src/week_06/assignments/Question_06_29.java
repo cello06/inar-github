6d91c30e97131aed93b3c98ccb437ce9b54b892d
Celalettin harzemşah Aktürk <celalettinakturk06@gmail.com>
2023-09-17 11:36:43 +0300

    Assignments of week_06

--- src/week_06/assignments/Question_06_29.java
+++ src/week_06/assignments/Question_06_29.java
@@ -2,6 +2,28 @@ package week_06.assignments;
 
 public class Question_06_29 {
     public static void main(String[] args) {
+        printTwinPrime();
+    }
 
+    public static void printTwinPrime() {
+        final int COUNT_OF_TWIN_PRIME = 1000;
+
+        int number = 2;
+
+        while (number <= COUNT_OF_TWIN_PRIME) {
+            if (isPrime(number) && isPrime(number + 2)) {
+                System.out.printf("(%d,%d)\n", number, number + 2);
+            }
+            number++;
+        }
+    }
+
+    public static boolean isPrime(int number) {
+        for (int i = 2; i <= number / 2; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
