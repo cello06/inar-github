7db13073e3d0add71cdc9973132c324dade5ffb8
Celalettin harzemşah Aktürk <celalettinakturk06@gmail.com>
2023-10-02 11:43:08 +0300

    assignments of week_07

---
+++ src/week_07/assignments/Question_07_03.java
@@ -0,0 +1,33 @@
+package week_07.assignments;
+
+import java.util.Scanner;
+
+public class Question_07_03 {
+    public static void main(String[] args) {
+        int[] numbers = getCountsOfNumbers();
+        printCountsOfNumbers(numbers);
+    }
+
+    public static int[] getCountsOfNumbers() {
+        Scanner input = new Scanner(System.in);
+        int[] numbers = new int[100];
+        System.out.print("Enter the integers between 1 and 100 : ");
+        for (int number : numbers) {
+            number = input.nextInt();
+            if (number == 0) {
+                break;
+            } else {
+                numbers[number % 100]++;
+            }
+        }
+        return numbers;
+    }
+
+    public static void printCountsOfNumbers(int[] list) {
+        for (int i = 0; i < list.length; i++) {
+            if (list[i] > 0) {
+                System.out.println(i + " occurs " + list[i] + ((list[i] > 1) ? " times" : " time"));
+            }
+        }
+    }
+}
