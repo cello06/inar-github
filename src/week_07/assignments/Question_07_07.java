899c0ed228be16e66bf61cb1168933caa72b8c35
Celalettin harzemşah Aktürk <celalettinakturk06@gmail.com>
2023-10-03 09:25:17 +0300

    assignments of week_07

---
+++ src/week_07/assignments/Question_07_07.java
@@ -0,0 +1,19 @@
+package week_07.assignments;
+
+public class Question_07_07 {
+    public static void main(String[] args) {
+        int[] counts = new int[10];
+
+        for (int i = 0; i < 100; i++) {
+            int randomNumber = (int) (Math.random() * 10);
+            counts[randomNumber % 10]++;
+        }
+        printCounts(counts);
+    }
+
+    public static void printCounts(int[] counts) {
+        for (int i = 0; i < counts.length; i++) {
+            System.out.println(i + "s : " + counts[i]);
+        }
+    }
+}
