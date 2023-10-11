267d79bd43ef499c319111c5c64b18b25ff27505
Celalettin harzemşah Aktürk <celalettinakturk06@gmail.com>
2023-10-11 12:02:16 +0300

    mistake erased

--- src/week_07/assignments/Question_07_17.java
+++ src/week_07/assignments/Question_07_17.java
@@ -4,50 +4,52 @@ import java.util.Scanner;
 
 public class Question_07_17 {
     public static void main(String[] args) {
-        evaluateStudents();
+        eSchool();
 
     }
 
-    public static void evaluateStudents() {
-        int countOfStudents = getCountOfStudents();
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("Enter the name a score for each student : ");
-
-        String[] namesAndScoresOfStudents = new String[countOfStudents * 2];
-        int[] scoresOfStudent = new int[countOfStudents];
-        int index = 0;
-        for (int i = 0; i < namesAndScoresOfStudents.length; i += 2) {
-            System.out.print("Student " + (index + 1) + " : ");
-            namesAndScoresOfStudents[i] = input.nextLine();
-            System.out.print("Score : ");
-            namesAndScoresOfStudents[i + 1] = input.nextLine();
-            scoresOfStudent[index] = Integer.parseInt(namesAndScoresOfStudents[i + 1]);
-            index++;
-        }
-        java.util.Arrays.sort(scoresOfStudent);
-        printNameOfStudentsAccordingToScores(namesAndScoresOfStudents, scoresOfStudent);
-
-    }
-
-
-    public static int getCountOfStudents() {
+    public static void eSchool() {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the number of students : ");
-        return input.nextInt();
+        int countOfStudents = input.nextInt();
+        System.out.println("Enter the name a score for each student: ");
+        String[] namesOfStudents = new String[countOfStudents];
+        int[] scoresOfStudents = new int[countOfStudents];
+        for (int i = 0; i < countOfStudents; i++) {
+            System.out.print("Student " + (i + 1) + " : ");
+            namesOfStudents[i] = input.next();
+            System.out.print("Score : ");
+            scoresOfStudents[i] = input.nextInt();
+        }
+        sort(namesOfStudents, scoresOfStudents);
+        printStudents(namesOfStudents);
     }
 
-    public static void printNameOfStudentsAccordingToScores
-            (String[] namesAndScoresOfStudents, int[] scoresOfStudent) {
-        int index = -1;
-        for (int i = scoresOfStudent.length - 1; i >= 0; i--) {
-            for (int j = 0; j < namesAndScoresOfStudents.length; j += 2) {
-                if (namesAndScoresOfStudents[j + 1].equals("" + scoresOfStudent[i])) {
+    public static void sort(String[] names, int[] scores) {
+        for (int i = 0; i < scores.length - 1; i++) {
+            int currentMax = scores[i];
+            int currentMaxIndex = i;
+            String currentMaxName = names[i];
 
-                        System.out.println(namesAndScoresOfStudents[j]);
+            for (int j = i + 1; j < scores.length; j++) {
+                if (scores[j] > currentMax) {
+                    currentMax = scores[j];
+                    currentMaxIndex = j;
+                    currentMaxName = names[j];
                 }
+            }
+            if (currentMaxIndex != i) {
+                scores[currentMaxIndex] = scores[i];
+                scores[i] = currentMax;
+                names[currentMaxIndex] = names[i];
+                names[i] = currentMaxName;
             }
         }
     }
+
+    public static void printStudents(String[] names) {
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
 }
-//if the scores are equals I have problems ! ! !
