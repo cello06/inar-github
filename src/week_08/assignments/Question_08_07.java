393cf047b4efcb9465517395f488267cf7632732
Celalettin harzemşah Aktürk <celalettinakturk06@gmail.com>
2023-10-15 10:48:33 +0300

    Assignments of week_08

--- src/week_08/assignments/Question_08_07.java
+++ src/week_08/assignments/Question_08_07.java
@@ -8,9 +8,45 @@ public class Question_08_07 {
         printClosestPoints(points);
 
     }
-    public static void printClosestPoints(double[][] points){
-        for (int row = 0; row < points.length; row++) {
+
+    public static void printClosestPoints(double[][] points) {
+        double shortestDistance = 0;
+        double resultX1 = 0;
+        double resultY1 = 0;
+        double resultZ1 = 0;
+        double resultX2 = 0;
+        double resultY2 = 0;
+        double resultZ2 = 0;
+        for (int row = 0; row < points.length - 1; row++) {
+            double x1 = points[row][0];
+            double y1 = points[row][1];
+            double z1 = points[row][2];
+            for (int secondRow = row + 1; secondRow < points.length; secondRow++) {
+                double x2 = points[secondRow][0];
+                double y2 = points[secondRow][1];
+                double z2 = points[secondRow][2];
+
+
+                if (row == 0) {
+                    shortestDistance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2) + Math.pow(z2 - z1, 2));
+                } else {
+                    double currentDistance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2) + Math.pow(z2 - z1, 2));
+                    if (shortestDistance > currentDistance) {
+                        shortestDistance = currentDistance;
+                        resultX1 = x1;
+                        resultY1 = y1;
+                        resultZ1 = z1;
+                        resultX2 = x2;
+                        resultY2 = y2;
+                        resultZ2 = z2;
+                    }
+                }
+            }
 
         }
+        System.out.println("The closest 2 points are"
+                + " (" + resultX1 + "," + resultY1 + "," + resultZ1 + ") and "
+                + " (" + resultX2 + "," + resultY2 + "," + resultZ2 + ")");
+        System.out.printf("Distance : %.2f", shortestDistance);
     }
 }
