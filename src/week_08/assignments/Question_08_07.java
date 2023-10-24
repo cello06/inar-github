ebf15d8706ba4976dd19f40a230aeefbe2a9cd33
Celalettin harzemşah Aktürk <celalettinakturk06@gmail.com>
2023-10-24 13:47:42 +0300

    some changes

--- src/week_08/assignments/Question_08_07.java
+++ src/week_08/assignments/Question_08_07.java
@@ -5,48 +5,40 @@ public class Question_08_07 {
         double[][] points = {{-1, 0, 3}, {-1, -1, -1}, {4, 1, 1},
                 {2, 0.5, 9}, {3.5, 2, -1}, {3, 1.5, 3}, {-1.5, 4, 2},
                 {5.5, 4, -0.5}};
-        printClosestPoints(points);
+        double[][] closestTwoPoint = new double[2][3];
+        findClosestPoints(points, closestTwoPoint);
+        print(closestTwoPoint);
 
     }
 
-    public static void printClosestPoints(double[][] points) {
-        double shortestDistance = 0;
-        double resultX1 = 0;
-        double resultY1 = 0;
-        double resultZ1 = 0;
-        double resultX2 = 0;
-        double resultY2 = 0;
-        double resultZ2 = 0;
-        for (int row = 0; row < points.length - 1; row++) {
-            double x1 = points[row][0];
-            double y1 = points[row][1];
-            double z1 = points[row][2];
-            for (int secondRow = row + 1; secondRow < points.length; secondRow++) {
-                double x2 = points[secondRow][0];
-                double y2 = points[secondRow][1];
-                double z2 = points[secondRow][2];
+    public static void findClosestPoints(double[][] points, double[][] closestTwoPoints) {
+        double closestDistance = Double.MAX_VALUE;
+        for (int firstPoint = 0; firstPoint < points.length - 1; firstPoint++) {
+            for (int otherPoint = firstPoint + 1; otherPoint < points.length; otherPoint++) {
 
-                double currentDistance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2) + Math.pow(z2 - z1, 2));
-
-                if (row == 0) {
-                    shortestDistance = currentDistance;
-                } else {
-                    if (shortestDistance > currentDistance) {
-                        shortestDistance = currentDistance;
-                        resultX1 = x1;
-                        resultY1 = y1;
-                        resultZ1 = z1;
-                        resultX2 = x2;
-                        resultY2 = y2;
-                        resultZ2 = z2;
-                    }
+                double currentDistance = findDistance(firstPoint, otherPoint, points);
+                if (currentDistance < closestDistance) {
+                    closestDistance = currentDistance;
+                    closestTwoPoints[0][0] = points[firstPoint][0];
+                    closestTwoPoints[0][1] = points[firstPoint][1];
+                    closestTwoPoints[0][2] = points[firstPoint][2];
+                    closestTwoPoints[1][0] = points[otherPoint][0];
+                    closestTwoPoints[1][1] = points[otherPoint][1];
+                    closestTwoPoints[1][2] = points[otherPoint][2];
                 }
             }
-
         }
-        System.out.println("The closest 2 points are"
-                + " (" + resultX1 + "," + resultY1 + "," + resultZ1 + ") and "
-                + " (" + resultX2 + "," + resultY2 + "," + resultZ2 + ")");
-        System.out.printf("Distance : %.2f", shortestDistance);
+    }
+
+    public static double findDistance(int firstPoint, int otherPoint, double[][] points) {
+        return Math.sqrt(Math.pow(points[otherPoint][0] - points[firstPoint][0], 2) +
+                Math.pow(points[otherPoint][1] - points[firstPoint][1], 2) +
+                Math.pow(points[otherPoint][2] - points[firstPoint][2], 2));
+    }
+
+    public static void print(double[][] closestTwoPoints) {
+        System.out.print("The closest two points are ("
+                + closestTwoPoints[0][0] + "," + closestTwoPoints[0][1] + "," + closestTwoPoints[0][2] + ") and (" +
+                closestTwoPoints[1][0] + "," + closestTwoPoints[1][1] + "," + closestTwoPoints[1][2] + ")");
     }
 }
