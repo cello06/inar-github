e5fadfb57ad3940f50de3ea6061d86d16055758d
Celalettin harzemşah Aktürk <celalettinakturk06@gmail.com>
2023-10-30 17:39:14 +0300

    assignments of week_09

--- src/week_09/assignments/Question_09_03.java
+++ src/week_09/assignments/Question_09_03.java
@@ -6,6 +6,7 @@ public class Question_09_03 {
     public static void main(String[] args) {
         for (long i = 10000; i <= 100000000000L; i *= 10) {
             Date date = new Date(i);
+
             System.out.println("For elapsed " + i + " time is " + date.toString());
         }
     }
