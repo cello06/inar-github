7352e3120a5acf17fff3dd6ac2d2a9cbb3a30952
Celalettin harzemşah Aktürk <celalettinakturk06@gmail.com>
2023-10-30 15:12:25 +0300

    assignments of week_09

---
+++ src/week_09/assignments/Stock.java
@@ -0,0 +1,17 @@
+package week_09.assignments;
+
+public class Stock {
+    String symbol;
+    String name;
+    double previousClosingPrice;
+    double currentPrice;
+
+    Stock(String symbol, String name) {
+        this.symbol = symbol;
+        this.name = name;
+    }
+
+    double getChangePercent() {
+        return ((currentPrice - previousClosingPrice) / previousClosingPrice) * 100;
+    }
+}
