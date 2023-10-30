5e82f43b296c1bfc76530138a2b16c3a4a617595
Celalettin harzemşah Aktürk <celalettinakturk06@gmail.com>
2023-10-30 17:47:33 +0300

    assignments of week_09

---
+++ src/week_09/assignments/Question_09_04.java
@@ -0,0 +1,18 @@
+package week_09.assignments;
+
+import java.util.Random;
+
+public class Question_09_04 {
+    public static void main(String[] args) {
+        Random random = new Random(1000);
+
+        for (int i = 0; i < 50; i++) {
+            int currentRandom = random.nextInt(100);
+            if((i+1)%10 == 0){
+                System.out.printf("%3d\n",currentRandom);
+            }else {
+                System.out.printf("%3d",currentRandom);
+            }
+        }
+    }
+}
