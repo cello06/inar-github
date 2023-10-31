66599163c685621cb370039f0466aaf9c2b1a7cd
Celalettin harzemşah Aktürk <celalettinakturk06@gmail.com>
2023-10-31 07:07:45 +0300

    assignments of week_09

---
+++ src/week_09/assignments/Account.java
@@ -0,0 +1,71 @@
+package week_09.assignments;
+
+import java.util.Date;
+
+public class Account {
+    private int id;
+    private double balance = 0;
+    private double annualInterestRate = 0;
+    private Date dateCreated;
+
+    Account() {
+        dateCreated = new Date();
+    }
+
+    Account(int id, double initialBalance) {
+        dateCreated = new Date();
+        this.id = id;
+        balance = initialBalance;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getAnnualInterestRate() {
+        return annualInterestRate;
+    }
+
+    public void setAnnualInterestRate(double annualInterestRate) {
+        this.annualInterestRate = annualInterestRate;
+    }
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public double getMonthlyInterestRate() {
+        return annualInterestRate / 1200;
+    }
+
+    public double getMonthlyInterest() {
+        return balance * getMonthlyInterestRate();
+    }
+
+    public void withdraw(double amount) {
+        if (balance > amount) {
+            balance -= amount;
+        } else {
+            System.out.println("Not enough money!");
+        }
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        } else {
+            System.out.println("Invalid amount to deposit");
+        }
+    }
+}
---
+++ src/week_09/assignments/Question_09_07.java
@@ -0,0 +1,27 @@
+package week_09.assignments;
+
+public class Question_09_07 {
+    public static void main(String[] args) {
+        int id = 1122;
+        double initialBalance = 20_000;
+        double annualInterestRate = 4.5;
+        double withdrawAmount = 2_500;
+        double depositAmount = 3_000;
+
+        Account account = new Account(id, initialBalance);
+        account.setAnnualInterestRate(annualInterestRate);
+        account.withdraw(withdrawAmount);
+        account.deposit(depositAmount);
+
+        printAccountSummary(account);
+    }
+
+    public static void printAccountSummary(Account account) {
+        System.out.println("           Account Statement : ");
+        System.out.println("-------------------------------------");
+        System.out.println("Account ID       : " + account.getId());
+        System.out.println("Date created     : " + account.getDateCreated().toString());
+        System.out.printf("Balance          : $%.2f\n", account.getBalance());
+        System.out.printf("Monthly Interest : $%.2f", account.getMonthlyInterest());
+    }
+}
