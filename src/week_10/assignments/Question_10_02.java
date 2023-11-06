2a5cfc566b4f1bc2150f6a7870bf52f76149a17e
Celalettin harzemşah Aktürk <celalettinakturk06@gmail.com>
2023-11-06 12:55:41 +0300

    week_09 assignments

---
+++ src/week_10/assignments/Question_10_02.java
@@ -0,0 +1,10 @@
+package week_10.assignments;
+
+public class Question_10_02 {
+    public static void main(String[] args) {
+        BMI bmi1 = new BMI("Martin",18,143,2,24);
+        System.out.println(bmi1);
+        BMI bmi2 = new BMI("Adam Smith",75,175);
+        System.out.println(bmi2);
+    }
+}
