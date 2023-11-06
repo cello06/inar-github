6240b772227b270ffc69d5d4c2e3131c17063c7d
Celalettin harzemşah Aktürk <celalettinakturk06@gmail.com>
2023-11-06 20:00:18 +0300

    week_09 assignments

---
+++ src/week_10/assignments/Question_10_05.java
@@ -0,0 +1,48 @@
+package week_10.assignments;
+
+import java.util.Scanner;
+
+public class Question_10_05 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a positive integer : ");
+        int number = input.nextInt();
+        StackOfIntegers stackOfIntegers = new StackOfIntegers(number);
+        for (int i = 2; i <= number; i++) {
+            if (isPrime(i) && number % i == 0) {
+                number = number / i;
+                stackOfIntegers.push(i);
+                i--;
+            }
+        }
+
+        printIncreasedOrder(stackOfIntegers);
+        System.out.println();
+        printDecreasingOrder(stackOfIntegers);
+
+
+    }
+
+    public static boolean isPrime(int number) {
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void printIncreasedOrder(StackOfIntegers stackOfIntegers) {
+        System.out.print("Increased order : ");
+        for (int i = 0; i < stackOfIntegers.getSize(); i++) {
+            System.out.print(stackOfIntegers.getPrimeFactors()[i] + " ");
+        }
+    }
+
+    public static void printDecreasingOrder(StackOfIntegers stackOfIntegers) {
+        System.out.print("Decreasing order: ");
+        while (!stackOfIntegers.empty()) {
+            System.out.print(stackOfIntegers.pop() + " ");
+        }
+    }
+}
---
+++ src/week_10/assignments/StackOfIntegers.java
@@ -0,0 +1,35 @@
+package week_10.assignments;
+
+public class StackOfIntegers {
+    private int[] primeFactors;
+    private int size;
+
+    public StackOfIntegers(int number) {
+        primeFactors = new int[number];
+    }
+
+    public void push(int number) {
+        if (size >= primeFactors.length) {
+            int[] temp = new int[primeFactors.length * 2];
+            System.arraycopy(primeFactors, 0, temp, 0, primeFactors.length);
+            primeFactors = temp;
+        }
+        primeFactors[size++] = number;
+    }
+
+    public int pop() {
+        return primeFactors[--size];
+    }
+
+    public boolean empty() {
+        return size == 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int[] getPrimeFactors() {
+        return primeFactors;
+    }
+}
