4cd30157199d79ef2c3ed7f172884d8b50845ccf
Celalettin harzemşah Aktürk <celalettinakturk06@gmail.com>
2023-11-06 12:38:22 +0300

    week_09 assignments

---
+++ src/week_10/assignments/Question_10_01.java
@@ -0,0 +1,10 @@
+package week_10.assignments;
+
+public class Question_10_01 {
+    public static void main(String[] args) {
+        Time time = new Time();
+        System.out.println(time);
+        Time elapsedTime = new Time(555550000);
+        System.out.println(elapsedTime);
+    }
+}
---
+++ src/week_10/assignments/Time.java
@@ -0,0 +1,50 @@
+package week_10.assignments;
+
+import java.util.Date;
+
+public class Time {
+    private int hour;
+    private int minute;
+    private int second;
+
+    public Time() {
+        long totalSecond = System.currentTimeMillis() / 1000;
+        second = (int) (totalSecond % 60);
+        minute = (int) ((totalSecond / 60) % 60);
+        hour = (int) ((totalSecond / 3600) % 24);
+    }
+
+    public Time(long elapsedTime) {
+        long totalSecond = elapsedTime / 1000;
+        second = (int) (totalSecond % 60);
+        minute = (int) ((totalSecond / 60) % 60);
+        hour = (int) ((totalSecond / 3600) % 24);
+    }
+
+    public Time(int hour, int minute, int second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public void setTime(long elapsedTime) {
+        new Time(elapsedTime);
+    }
+
+    @Override
+    public String toString() {
+        return hour + ":" + minute + ":" + second;
+    }
+}
