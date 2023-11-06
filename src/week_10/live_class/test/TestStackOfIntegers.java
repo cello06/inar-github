5abbec9a2da9923205a929bb91450b22097fe48e
Celalettin harzemşah Aktürk <celalettinakturk06@gmail.com>
2023-11-06 10:35:36 +0300

    week_09 live_class

---
+++ src/week_10/live_class/test/TestStackOfIntegers.java
@@ -0,0 +1,16 @@
+package week_10.live_class.test;
+
+import week_10.live_class.classes.StackOfIntegers;
+
+public class TestStackOfIntegers {
+    public static void main(String[] args) {
+        StackOfIntegers stack = new StackOfIntegers();
+        for (int i = 0; i < 10; i++) {
+            stack.push(i);
+        }
+        while (!stack.empty()) {
+            System.out.print(stack.pop() + " ");
+        }
+
+    }
+}
