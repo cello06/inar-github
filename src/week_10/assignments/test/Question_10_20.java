053260535ee607f11417d95dfcc9abfba320eae4
Celalettin harzemşah Aktürk <celalettinakturk06@gmail.com>
2023-11-08 21:32:11 +0300

    week_09 assignments

--- src/week_10/assignments/test/Question_10_20.java
+++ src/week_10/assignments/test/Question_10_20.java
@@ -9,20 +9,22 @@ public class Question_10_20 {
         System.out.println("i                      e");
         System.out.println("------------------------------------");
 
-        for (int i = 100; i <= 1000 ; i += 100) {
+        for (int i = 100; i <= 1000; i += 100) {
             BigDecimal e = formula(i);
-            System.out.println(i+"    "+e);
+            System.out.println(i + "    " + e);
         }
     }
-    public static BigDecimal formula(int i){
+
+    public static BigDecimal formula(int i) {
         BigDecimal result = new BigDecimal(1);
 
         for (int j = 1; j < i; j++) {
-            result = result.add(BigDecimal.ONE.divide(factorial(j),25, RoundingMode.UP));
+            result = result.add(BigDecimal.ONE.divide(factorial(j), 25, RoundingMode.UP));
         }
-       return result;
+        return result;
     }
-    public static BigDecimal factorial(int value){
+
+    public static BigDecimal factorial(int value) {
         BigDecimal result = new BigDecimal(1);
         for (int i = 0; i < value; i++) {
             result = result.multiply(BigDecimal.valueOf(i).add(BigDecimal.ONE));
