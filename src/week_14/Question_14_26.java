3cbf1d482f226940439961745acb8fa67af6f5b7
Celalettin harzemşah Aktürk <celalettinakturk06@gmail.com>
2023-12-10 01:46:40 +0300

    assignments

---
+++ src/week_14/Question_14_26.java
@@ -0,0 +1,45 @@
+package week_14;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Question_14_26 {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        int sizeOfList = 5;
+        for (int i = 0; i < sizeOfList; i++) {
+            int random = (int) (Math.random() * 100 + 1);
+            list.add(random);
+        }
+        System.out.println("Original Linked List : \n" + list);
+        Queue<Integer> queue = new LinkedList<>();
+        int count = 0;
+        for (int i = 0; i < sizeOfList; i++) {
+            queue.add(list.get(i + count));
+            int currentGCD;
+
+            if (queue.size() == 2) {
+                int firstNumber = queue.remove();
+                int secondNumber = queue.peek();
+                currentGCD = gcd(firstNumber, secondNumber);
+                list.add(i + count, currentGCD);
+                count++;
+            }
+
+        }
+        System.out.println("Updated Linked List : \n" + list);
+    }
+
+    public static int gcd(int first, int second) {
+        int lowerNumber = Math.min(first, second);
+        int possibleGCD = 2;
+        int number = 1;
+        while (number <= lowerNumber) {
+            if (first % number == 0 && second % number == 0) {
+                possibleGCD = number;
+            }
+            number++;
+        }
+        return possibleGCD;
+    }
+}
